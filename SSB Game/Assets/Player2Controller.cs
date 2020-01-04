3e2a5d56c14344c795b65a20c16c258e261129a8
anotherguy69 <59512526+anotherguy69@users.noreply.github.com>
2020-01-04 19:19:04 +0100

    the game itself

---
+++ SSB Game/Assets/Player2Controller.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour
+{
+    [Header("Movement Variables")]
+    public float speed;
+    public Rigidbody2D rb;
+    public Animator anim;
+    public bool isFacingRight;
+
+    [Header("Jump Variables")]
+    public float jumpForce;
+    public bool isJumping;
+    public bool isGrounded;
+    public LayerMask groundMask;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    private int startHowManyJumps;
+    public int howManyJumps;
+
+
+    void Update()
+    {
+     
+        MovementCheck();
+        JumpCheck();
+
+    }
+
+
+    void MovementCheck()
+    {
+
+        float horizontal = Input.GetAxis("HorizontalConsole");
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        if (horizontal < 0)
+        {
+            isFacingRight = false;
+        }
+        else if (horizontal > 0)
+        {
+            isFacingRight = true;
+        }
+    }
+
+
+    void JumpCheck()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundMask);
+
+        if (isGrounded == true)
+        {
+            isJumping = false;
+
+            startHowManyJumps = howManyJumps;
+        }
+
+        if (Input.GetButtonDown("Jump") && startHowManyJumps > 0 && isGrounded == true)
+        {
+
+            isJumping = true;
+            startHowManyJumps--;
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce);
+        }
+        else if (Input.GetButtonDown("Jump") && startHowManyJumps > 0)
+        {
+            isJumping = true;
+
+            startHowManyJumps--;
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce);
+        }
+    }
+}
---
+++ SSB Game/Assets/PlayerController3D.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController3D : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed;
+    public float gravity;
+    public float jumpForce;
+
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void MovementCheck()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 move = transform.right * horizontal;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+    }
+
+    void JumpCheck()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+        
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+        }
+
+    }
+}
